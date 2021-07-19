15621173652919a3b8088c590756fd86c33589c4
csh <ttlmazin112@naver.com>
2021-07-19 14:55:49 +0900

    React Crona Project

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        
+    <header className="header">
+      <h1>COVID-19</h1>
+      <select>
+        <option value="kr">국내</option>
+        <option value="world">세계</option>
+      </select>
+    </header>
+       
+    )
+}
+
+export default Header
---
+++ src/world.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './World.css';
+
+function World() {
+  return (
+    <div className="World">
+      <Header/>
+      <Contents/>
+    </div>
+  );
+}
+
+export default World;
